156e2abc30f94357a37580fe562fcb6db7943e62
Tyler Ruff <hello@blazed.space>
2022-08-16 14:40:02 -0400

    BLZ

--- assets/js/site.blz.js
+++ assets/js/site.blz.js
@@ -50,60 +50,6 @@ function hide_burger(event){
     document.getElementById('header-mobile').classList.add('hidden');
 }
 
-function hide_user_panel(event = null){
-	document.getElementById('user-panel').classList.add('hidden');
-	document.getElementById('toggle-user-bar').innerHTML = "My Blazed";
-	document.getElementById('user-bar-icon').innerHTML = `<svg fill="#ffffff" class="inline-flex w-4 h-4" xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512.02 319.26"><path d="M5.9 48.96 48.97 5.89c7.86-7.86 20.73-7.84 28.56 0l178.48 178.48L434.5 5.89c7.86-7.86 20.74-7.82 28.56 0l43.07 43.07c7.83 7.84 7.83 20.72 0 28.56l-192.41 192.4-.36.37-43.07 43.07c-7.83 7.82-20.7 7.86-28.56 0l-43.07-43.07-.36-.37L5.9 77.52c-7.87-7.86-7.87-20.7 0-28.56z"/></svg>`;
-}
-
-function toggle_user_panel(event = null){
-	if(document.getElementById('user-panel').classList.contains('hidden')){
-		document.getElementById('user-panel').classList.remove('hidden');
-		document.getElementById('toggle-user-bar').innerHTML = "Close";
-		document.getElementById('user-bar-icon').innerHTML = `<svg fill="#ffffff" class="inline-flex w-4 h-4" xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 312.36"><path fill-rule="nonzero" d="M0 276.77 253.12 0 512 282.48l-32.65 29.88-226.2-246.83L32.66 306.64z"/></svg>`;
-		document.getElementById('main-content').addEventListener('mouseup', hide_user_panel);
-	} else {
-		hide_user_panel();
-	}
-}
-
-function openLoginModal(event){
-    document.body.classList.add('overflow-hidden');
-    document.getElementById('login-form').classList.remove('hidden');
-    document.getElementById('login-bg').addEventListener('mouseup', closeLoginModal);
-}
-
-function closeLoginModal(event = null){
-    document.body.classList.remove('overflow-hidden');
-    document.getElementById('login-form').classList.add('hidden');
-}
-
-function  build_user_bar(){
-	const userBar = f('div');
-	userBar.id = "user-panel";
-    userBar.classList.add('hidden', 'justify-between', 'flex', 'bg-gray-400', 'border-b', 'border-gray-800', 'py-5', 'pl-3', 'md:px-10');
-    const authStatus = f('span');
-    authStatus.classList.add('justify-start', 'inline-flex');
-    authStatus.id = "auth-status";
-    authStatus.innerHTML = 'Not logged in.';
-    const authWrapper = f('div');
-    authWrapper.id = "auth-wrapper";
-    authWrapper.classList.add('justify-end', 'inline-flex');
-    const login = f('a');
-    login.href = "#Top";
-    login.innerHTML = "Login";
-    login.addEventListener('mouseup', openLoginModal);
-    login.classList.add('px-3', 'cursor-pointer', 'hover:underline');
-    const register = f('a');
-    register.href = "https://blazed.world/register.html";
-    register.innerHTML = "Register";
-    register.classList.add('px-3', 'hover:underline');
-    authWrapper.appendChild(login);
-    authWrapper.appendChild(register);
-    userBar.appendChild(authStatus);
-    userBar.appendChild(authWrapper);
-    return userBar;
-}
 function build_menu(config, page){
     const header = f('header');
     header.classList.add('header', 'bg-gray-900');
@@ -176,8 +122,6 @@ function build_menu(config, page){
     innerMoreWrapper.appendChild(closeButtonWrapper);
     innerWrapper.appendChild(innerMoreWrapper);
     header.appendChild(innerWrapper);
-    const userBar = build_user_bar();
-    header.appendChild(userBar);
     return header;
 }
 
